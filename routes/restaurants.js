71693acddb5e7c5e4cb988d465523e22ba1cc0db
Hashim <hashimrasheed1999@gmail.com>
2021-01-15 10:27:56 +0530

    fixed error in landing page

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,6 +3,7 @@ const app = express.Router()
 const restaurants = require('../app/http/controllers/restaurants')
 const gradeAndCuisineController = require('../app/http/controllers/gradeAndCuisineController')
 
+app.get('/?:limit', restaurants().getRestaurants)
 app.get('/restaurants?:limit', restaurants().getRestaurants)
 app.get('/restaurant/:id', restaurants().getARestaurant)
 app.get('/getrestaurantgrade/:id', gradeAndCuisineController().getrestaurantgrade)
